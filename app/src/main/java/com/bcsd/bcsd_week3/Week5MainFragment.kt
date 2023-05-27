c3748ca87be18a435ba34bb30c01c2ea20d9bbe1
Thirfir <sangbbang31@gmail.com>
2023-05-27 16:11:54 +0900

    Week8 Notification
    Week5 fragment에서 알림 띄우고, 알림 클릭 시 Week4RandomActivity를 띄워....야 하는데 알림 클릭해도 액티비티가 안뜨는데 원인을 못찾겠습니다.....

--- app/src/main/java/com/bcsd/bcsd_week3/Week5MainFragment.kt
+++ app/src/main/java/com/bcsd/bcsd_week3/Week5MainFragment.kt
@@ -1,13 +1,19 @@
 package com.bcsd.bcsd_week3
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.os.bundleOf
+import androidx.core.app.NotificationCompat
+import androidx.fragment.app.Fragment
 import com.bcsd.bcsd_week3.databinding.FragmentWeek5MainBinding
 
 
@@ -19,6 +25,12 @@ class Week5MainFragment : Fragment() {
     ): View {
         binding = FragmentWeek5MainBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textCountNumber.text = "0"
         var count = 0
 
         binding.buttonToast.setOnClickListener {
@@ -38,11 +50,43 @@ class Week5MainFragment : Fragment() {
         }
 
         binding.buttonRandom.setOnClickListener {
-            requireActivity().supportFragmentManager.beginTransaction()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notificationIntent = Intent(requireContext(), Week4RandomActivity::class.java).apply {
+                    putExtra("count", count)
+                }
+                val notificationPendingIntent = PendingIntent
+                    .getActivity(
+                        requireContext(),
+                        NOTIFICATION_ID,
+                        notificationIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    )
+
+                val channelId = getString(R.string.channel_id)
+                val channelName = getString(R.string.channel_name)
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(channelId, channelName, importance)
+
+                NotificationCompat.Builder(requireContext(), channelId)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("Random Number")
+                    .setContentText("What is the random number?????")
+                    .setDefaults(NotificationCompat.DEFAULT_ALL)
+                    .setContentIntent(notificationPendingIntent)
+                    .setAutoCancel(true)
+                .also {
+                    val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notificationManager.createNotificationChannel(channel)
+                    notificationManager.notify(NOTIFICATION_ID, it.build())
+                }
+            }
+
+            /*requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.layout_week5_activity, Week5Fragment().apply {
                     arguments = bundleOf("count" to count)
-                }, "tag").addToBackStack("tag").commit()
+                }, "tag").addToBackStack("tag").commit()*/
         }
-        return binding.root
     }
 }
+
+const val NOTIFICATION_ID = 0
